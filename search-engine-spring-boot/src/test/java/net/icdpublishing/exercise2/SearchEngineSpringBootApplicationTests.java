78718da371f3bd2867694576253a267d53f722ac
Milko Galev <milkog@riscogroup.com>
2018-02-01 15:49:36 +0200

    Adding initial spring boot data

---
+++ search-engine-spring-boot/src/test/java/net/icdpublishing/exercise2/SearchEngineSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package net.icdpublishing.exercise2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchEngineSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
