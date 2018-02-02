b1bc8da2659a24620c0b569a420b4d52d4228771
Milko Galev <milkopg@yahoo.com>
2018-02-03 00:30:30 +0200

    Adding filtering by customerType

--- search-engine-spring-boot/src/main/java/net/icdpublishing/exercise2/myapp/customers/service/CustomSearchEngineRetrievalServiceImpl.java
+++ search-engine-spring-boot/src/main/java/net/icdpublishing/exercise2/myapp/customers/service/CustomSearchEngineRetrievalServiceImpl.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 
 import net.icdpublishing.exercise2.myapp.SearchRequest;
 import net.icdpublishing.exercise2.myapp.customers.domain.Customer;
+import net.icdpublishing.exercise2.myapp.customers.domain.CustomerType;
 import net.icdpublishing.exercise2.searchengine.domain.Record;
+import net.icdpublishing.exercise2.searchengine.domain.SourceType;
 import net.icdpublishing.exercise2.searchengine.loader.DataLoader;
 import net.icdpublishing.exercise2.searchengine.requests.SimpleSurnameAndPostcodeQuery;
 
@@ -23,7 +25,7 @@ public class CustomSearchEngineRetrievalServiceImpl implements CustomSearchEngin
 		String surname = query.getSurname();
 		DataLoader loader = new DataLoader();
 		Collection<Record> records = loader.loadAllDatasets()
-				.stream().filter( rec -> (rec.getPerson().getSurname().equals(surname) && rec.getPerson().getAddress().getPostcode().equals(postCode)))
+				.stream().filter( rec -> (rec.getPerson().getSurname().startsWith(surname) && rec.getPerson().getAddress().getPostcode().startsWith(postCode)))
 				.collect(Collectors.toList());
 		return records;
 	}
@@ -32,15 +34,30 @@ public class CustomSearchEngineRetrievalServiceImpl implements CustomSearchEngin
 	public Collection<Record> performSearch(String surname, String postcode, String email) {
 		SimpleSurnameAndPostcodeQuery query = new SimpleSurnameAndPostcodeQuery(surname, postcode);
 		Customer customer = daoService.findCustomerByEmailAddress(email);
+		CustomerType customerType = customer.getCustomType();
 		SearchRequest request = new SearchRequest(query, customer);
 		
 		Collection<Record> persons = handleRequest(request);
+		persons = persons.stream().filter(rec -> {
+			return isAllowedDisplayData(customerType, rec);
+		}).collect(Collectors.toList());
+		
 		return persons;
 	}
 	
 	private Collection<Record> handleRequest(SearchRequest request) {
 		Collection<Record> resultSet = getResults(request.getQuery());
 		return resultSet;
+	}
+	
+	private boolean isAllowedDisplayData(CustomerType customerType, Record record) {
+		boolean allowed = false;
+		if (CustomerType.PREMIUM.equals(customerType)) {
+			allowed = true;
+		} else if (CustomerType.NON_PAYING.equals(customerType) && SourceType.BT.equals(record.getSourceTypes())){
+			allowed = true;
+		}
+		return allowed;
 	}
 	
 	private Collection<Record> getResults(SimpleSurnameAndPostcodeQuery query) {
